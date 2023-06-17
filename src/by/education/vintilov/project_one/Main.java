2a32e39fecbe503af92b9c2f6b2b55af9d334b8e
[Vintarium] <[Vintarium77@gmail.com]>
2023-06-17 10:50:08 +0300

    Sent email to the Ilon Musk

--- src/by/education/vintilov/project_one/Main.java
+++ src/by/education/vintilov/project_one/Main.java
@@ -2,6 +2,6 @@ package by.education.vintilov.project_one;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world_1");
+        System.out.println("how do you like this, Elon Musk?");
     }
 }
