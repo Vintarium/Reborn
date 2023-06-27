16dc9b8c5fb03601ed8a869a88d890c434235a9d
[Vintarium] <[Vintarium77@gmail.com]>
2023-06-27 23:00:10 +0300

    Add method getBalanceInOtherCurrencies()

---
+++ src/by/education/vintilov/ATM/java/DebitCard.java
@@ -0,0 +1,37 @@
+package by.education.vintilov.ATM.java;
+
+public class DebitCard {
+    private int id;
+    private String name;
+    private int balance;
+
+    public DebitCard(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+}
