7ba3b6434bb707c05c0bdc26bc0f9340b8c22a88
[Vintarium] <[Vintarium77@gmail.com]>
2023-08-12 21:53:26 +0300

    Added  code and test code to new Project(Find Median Value)

---
+++ src/by/education/vintilov/Finding_the_Median/java/Main.java
@@ -0,0 +1,14 @@
+package by.education.vintilov.Finding_the_Median.java;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double[] arr = new double[]{0.1, 0.2, 0.3, 0.4, 0.2, 0.5};
+
+
+        MedianValue medianValue = new MedianValue();
+        System.out.println(medianValue.findMediana(arr));
+
+
+    }
+}
